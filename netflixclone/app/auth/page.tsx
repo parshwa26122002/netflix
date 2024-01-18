16432277336465237234d9352d4b2fd086d09b87
Parshwa Shah <parshwa26122002@gmail.com>
2024-01-18 07:09:14 +0530

    Auth Page started

---
+++ netflixclone/app/auth/page.tsx
@@ -0,0 +1,9 @@
+export default function Auth() {
+    return (
+        <div className="relative bg-[url(/images/hero.jpg)] bg-no-repeat bg-center bg-fixed bg-cover w-full h-full">
+            <div className="bg-black w-full h-full lg:bg-opacity-50">
+                
+            </div>
+        </div>
+    )
+}
